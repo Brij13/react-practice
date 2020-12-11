d03255d4f81ee2545530c158a9e53431721d714d
Brij13 <brij.mandaliya1998@gmail.com>
2020-12-11 15:29:50 +0530

    Counter module add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Hello from './component/first_component';
 import './App.css';
 import Welcome from './component/class_component';
 import Message from './component/Message';
+import Counter from './component/Counter';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Message />
+        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react'
 
 class Counter extends Component {
+	constructor(){
+		super()
+		this.state = {
+			count : 0
+		}
+	}
+	increment(){
+		this.setState({
+		count : this.state.count+1
+		})
+		console.log(this.state.count);
+	}
+	
 	render(){
 		return(
-			<div></div>
+			<div>
+			<p>Count - {this.state.count}</p>
+			<button onClick={()=> this.increment() }> Increment </button>
+			</div>
 		)
 	}
 }
